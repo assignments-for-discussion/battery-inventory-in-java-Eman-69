abbea9a3cae5a4c23e340e4406967c5ba12ec0a0
Eman-69 <92877747+Eman-69@users.noreply.github.com>
2023-12-03 12:25:59 +0000

    Update Main.java

--- bunchbysoh/Main.java
+++ bunchbysoh/Main.java
@@ -11,21 +11,21 @@ public class Main {
     CountsBySoH counts = new CountsBySoH();
     for(int i=0;i<presentCapacities.length;i++)
       {
-        double soh=(double)presentCapacities[i]/120;
+        double soh=(double)presentCapacities[i]/120; //rated capacities 120Ah
         soh=soh*100;
-        if(soh>80)      
+        if(soh>80)                                  //at 80% it is 96Ah- 100% it is 120Ah
           counts.healthy++;
-        else if(soh>=62)
+        else if(soh>=62)                            //at 62% it is 74.4Ah
           counts.exchange++;
         else
           counts.failed++;
       }
-    return counts;
+    return counts;                                  //returns the counts of healthy, exchangeable and failed batteries 
   }
 
   static void testBucketingByHealth() {
     System.out.println("Counting batteries by SoH...\n");
-    int[] presentCapacities = {113, 116, 80, 95, 92, 70};
+    int[] presentCapacities = {113, 116, 80, 95, 92, 70};           //sample test case
     CountsBySoH counts = countBatteriesByHealth(presentCapacities);
     assert(counts.healthy == 2);
     assert(counts.exchange == 3);
